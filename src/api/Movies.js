1bcd813ec918a6eea83b5c5b89840b633db55bf6
Justine Francis <jfvalmores@gmail.com>
2020-05-31 17:16:22 +0800

    react commit

---
+++ src/api/Movies.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import {
+  API_URL,
+  API_KEY,
+} from './config';
+
+const Movies = () => {
+  function searchMovies() {
+
+  }
+
+  return {
+    searchMovies
+  };
+}
+
+export default Movies;
